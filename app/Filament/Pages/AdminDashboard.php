e14ffc3e0eeb61242ab33461d487d821fc5db341
Ronaldo Kereh <kreh.whoami@gmail.com>
2025-01-13 17:13:51 +0800

    feat(filament): add admin dashboard page

---
+++ app/Filament/Pages/AdminDashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Dashboard;
+
+class AdminDashboard extends Dashboard
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $title = 'Dashboard';
+    // protected static string $view = 'filament.pages.admin-dashboard';
+}
