8f1d8ac15911ff77740b3ab496e32d0d9aab5344
Ronaldo Kereh <kreh.whoami@gmail.com>
2025-01-13 13:01:50 +0800

    feat(filament): add biaya pendaftaran, data, prodi resources

---
+++ app/Filament/Resources/BiayaPendaftaranResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BiayaPendaftaranResource\Pages;
+use App\Filament\Resources\BiayaPendaftaranResource\RelationManagers;
+use App\Models\BiayaPendaftaran;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BiayaPendaftaranResource extends Resource
+{
+    protected static ?string $model = BiayaPendaftaran::class;
+    protected static ?string $navigationIcon = 'heroicon-o-wallet';
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('biaya')->numeric()->step(100),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('biaya')->money('IDR'),
+                TextColumn::make('created_at')->dateTime(),
+                TextColumn::make('updated_at')->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBiayaPendaftarans::route('/'),
+            'create' => Pages\CreateBiayaPendaftaran::route('/create'),
+            'edit' => Pages\EditBiayaPendaftaran::route('/{record}/edit'),
+        ];
+    }
+
+    public static function canCreate(): bool {
+        return false;
+    }
+}
---
+++ app/Filament/Resources/ProgramStudiResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProgramStudiResource\Pages;
+use App\Filament\Resources\ProgramStudiResource\RelationManagers;
+use App\Models\ProgramStudi;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProgramStudiResource extends Resource
+{
+    protected static ?string $model = ProgramStudi::class;
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama')->label('Program Studi'),
+                TextColumn::make('created_at')
+                    ->label('Dibuat Pada')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProgramStudis::route('/'),
+            'create' => Pages\CreateProgramStudi::route('/create'),
+            'edit' => Pages\EditProgramStudi::route('/{record}/edit'),
+        ];
+    }
+}
